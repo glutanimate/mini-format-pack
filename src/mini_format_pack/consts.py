9256cff3400ed1326262c813cda53f3be022087b
Glutanimate <glutanimate@users.noreply.github.com>
2021-04-17 02:52:49 +0200

    Drop legacy 2.0 code

--- src/mini_format_pack/consts.py
+++ src/mini_format_pack/consts.py
@@ -9,14 +9,6 @@ Copyright: (c) 2018 Glutanimate <https://glutanimate.com/>
 License: GNU AGPLv3 <https://www.gnu.org/licenses/agpl.html>
 """
 
-import sys
 import os
-from anki import version
 
-anki21 = version.startswith("2.1.")
-sys_encoding = sys.getfilesystemencoding()
-
-if anki21:
-    addon_path = os.path.dirname(__file__)
-else:
-    addon_path = os.path.dirname(__file__).decode(sys_encoding)
+addon_path = os.path.dirname(__file__)
